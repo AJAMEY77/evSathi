954db979f94f9bb5348e287957ae1b0f45c2d155
Ashish Ram <eng.ashish9@gmail.com>
2024-03-10 11:40:54 +0530

    Added signup login forgot pass page

--- evsathi/lib/main.dart
+++ evsathi/lib/main.dart
@@ -1,24 +1,35 @@
+import 'package:evsathi/screen/Auth/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'evSathi',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+    var brightness = MediaQuery.of(context).platformBrightness;
 
+    var themeMode =
+        brightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light;
+
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.indigo,
+        // ... other theme configurations
+      ),
+      themeMode: themeMode,
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => const Login(),
+      },
     );
   }
 }
