45912220bf146145cbd3b2666f82671b9240c2ef
Amey Jadhav <ameyjadhav5704@gmail.com>
2024-03-17 12:05:34 +0530

    task

--- evsathi/lib/main.dart
+++ evsathi/lib/main.dart
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
 // import 'package:evsathi/screens/booking_page.dart';
 import 'package:evsathi/screens/featurePage/featurePage.dart';
 import 'package:evsathi/screens/statusPage/status.dart';
 // import 'package:evsathi/screens/status.dart';
-=======
+
 import 'package:evsathi/screen/AdminPage/loginPage.dart';
 import 'package:evsathi/screen/Auth/login.dart';
-import 'package:evsathi/screen/googleMap/map.dart';
 import 'package:evsathi/screen/paymentPage/upi.dart';
->>>>>>> 8847e9243f1e67128b296efe184d4de3105fb3bc
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -19,21 +17,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MaterialApp(
-      title: 'evSathi',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: featurePage(),
-=======
     var brightness = MediaQuery.of(context).platformBrightness;
 
     var themeMode =
         brightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light;
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -50,7 +37,6 @@ class MyApp extends StatelessWidget {
       routes: {
         'loginPage': (context) => const LoginPage(),
       },
->>>>>>> 8847e9243f1e67128b296efe184d4de3105fb3bc
     );
   }
 }
